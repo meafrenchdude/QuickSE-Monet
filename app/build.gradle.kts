744cc0863f229338bed05fe61908d4adefdf314e
maazm7d <skmaaz933628@gmail.com>
2025-06-22 18:37:48 +0530

    v2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.maazm7d.quickse"
         minSdk = 25
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.1.1"
     }
 
     signingConfigs {
